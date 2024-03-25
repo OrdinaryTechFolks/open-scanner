3a0a64de5ac51d3db3bc9b75917398f7e9036789
Husein Nashr <nvdhunter007@gmail.com>
2024-03-25 21:52:47 +0700

    add sqlite on flutter web

--- lib/pkg/sqlite_client.dart
+++ lib/pkg/sqlite_client.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 class SQLiteClient {
   final String name;
@@ -11,28 +13,32 @@ class SQLiteClient {
   SQLiteClient(this.name, this.version, this.migrations);
 
   Future<void> initDatabase() async {
-    final dbPath = join(await getDatabasesPath(), name);
-    db = await openDatabase(
+    DatabaseFactory factory = kIsWeb ? databaseFactoryFfiWeb : databaseFactory;
+
+    final dbPath = join(await factory.getDatabasesPath(), name);
+    db = await factory.openDatabase(
       dbPath,
-      onCreate: (Database db, int version) async {
-        for (int i = 1; i <= version; i++) {
-          if (migrations[i] == null) continue;
-          await db.execute(migrations[i]!.$1);
-        }
-      },
-      onUpgrade: (db, oldVersion, newVersion) async {
-        for (int i = oldVersion + 1; i <= newVersion; i++) {
-          if (migrations[i] == null) continue;
-          await db.execute(migrations[i]!.$1);
-        }
-      },
-      onDowngrade: (db, oldVersion, newVersion) async {
-        for (int i = oldVersion; i > newVersion; i--) {
-          if (migrations[i] == null) continue;
-          await db.execute(migrations[i]!.$2);
-        }
-      },
-      version: version,
+      options: OpenDatabaseOptions(
+        onCreate: (Database db, int version) async {
+          for (int i = 1; i <= version; i++) {
+            if (migrations[i] == null) continue;
+            await db.execute(migrations[i]!.$1);
+          }
+        },
+        onUpgrade: (db, oldVersion, newVersion) async {
+          for (int i = oldVersion + 1; i <= newVersion; i++) {
+            if (migrations[i] == null) continue;
+            await db.execute(migrations[i]!.$1);
+          }
+        },
+        onDowngrade: (db, oldVersion, newVersion) async {
+          for (int i = oldVersion; i > newVersion; i--) {
+            if (migrations[i] == null) continue;
+            await db.execute(migrations[i]!.$2);
+          }
+        },
+        version: version,
+      ),
     );
   }
 
@@ -40,8 +46,8 @@ class SQLiteClient {
     await db.execute(query, arguments);
   }
 
-  Future<List<Map<String, Object?>>> query(
-      String query, {List<Object>? arguments}) async {
+  Future<List<Map<String, Object?>>> query(String query,
+      {List<Object>? arguments}) async {
     return await db.rawQuery(query, arguments);
   }
 }
